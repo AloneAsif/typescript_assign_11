f72098c1dd5eaa542bc00a3869f8cb513dcee88c
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-09 14:58:16 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question no # 12    Names iteration on array
+let friends_list = ['asim', 'atif', 'ali', 'danish']; //friends names in one array
+for (let i of friends_list) { // applying iteration on array 
+    console.log(`Hi how are you, ${friends_list}`);
+}
---
+++ main.ts
@@ -0,0 +1,6 @@
+// Question no # 12    Names iteration on array
+let friends_list: string[] = ['asim', 'atif', 'ali', 'danish'];  //friends names in one array
+
+for(let i of friends_list){       // applying iteration on array 
+    console.log(`Hi how are you, ${friends_list}`);     
+}
